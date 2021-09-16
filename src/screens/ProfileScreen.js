7fa3b3161625b27d8bfb8aa09372a54bf4272fce
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-09-16 20:51:04 +0300

    Test

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,12 +4,14 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import PlansScreen from "../screens/PlansScreen";
+import Nav from "../Nav";
 
 function ProfileScreen() {
   const user = useSelector(selectUser);
 
   return (
     <div className="profileScreen">
+      <Nav />
       <div className="profileScreen__body">
         <h1>Edit Profile</h1>
         <div className="profileScreen__info">
